88f3099f16deaa6f85e698ac2592db278cb4294c
Gonron <lundsgaard1997@hotmail.dk>
2019-03-14 11:05:29 +0100

    part01 done

---
+++ part01.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+    
+@app.route('/', methods = ['POST'])
+def api_message():
+    SERVER_PASSWORD = "aA"
+    if request.headers['Content-Type'] == 'application/json':
+        for k, v in request.get_json().items():
+            client_password=v
+        if client_password == SERVER_PASSWORD:
+            return "Access Granted", 200
+        else:
+            return "Access Denied", 403
+    else:
+        return "415 Unsupported Media Type"
